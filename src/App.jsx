04c740fa0a5c2edfb9a38a5d1b826262ef443527
RedJyu <piotr.stezycki@protonmai.com>
2023-09-18 14:23:29 +0200

    lorem challenge finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,47 @@
+import { useState } from 'react';
+import data from './data';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState('');
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+  };
+  return (
+    <div>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <div>
+          <h4>ipsum</h4>
+          <label htmlFor='count' className='lorem-label'>
+            Paragraphs:
+          </label>
+          <input
+            type='number'
+            name='count'
+            id='count'
+            value={count}
+            min={1}
+            max={8}
+            step={1}
+            className='lorem-input'
+            onChange={handleChange}
+          />
+        </div>
+        <button type='submit' className='btn lorem-btn'>
+          generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((lorem, index) => {
+          return <p key={index}>{lorem}</p>;
+        })}
+      </article>
+    </div>
+  );
 };
 export default App;
